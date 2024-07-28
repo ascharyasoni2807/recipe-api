bf1a616eec2a26f54b0f500f7edd3ab837d259b0
ascharyasoni2807 <65384919+ascharyasoni2807@users.noreply.github.com>
2024-07-28 13:16:56 +0530

    added logger in test cases

--- recipe/test.py
+++ recipe/test.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from django.contrib.auth import get_user_model
@@ -7,6 +8,9 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 class RecipeAPITestCase(APITestCase):
 
     def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
             username='testuser',
@@ -31,11 +35,13 @@ class RecipeAPITestCase(APITestCase):
         self.client.force_authenticate(user=self.user)
 
     def test_recipe_list(self):
+        self.logger.info('Testing recipe list endpoint')
         response = self.client.get('/api/recipe/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
 
     def test_recipe_create(self):
+        self.logger.info('Testing recipe creation endpoint')
         url = '/api/recipe/create/'
         data = {
             'category': {"name":str(self.category)}, 
@@ -51,18 +57,21 @@ class RecipeAPITestCase(APITestCase):
         self.assertEqual(Recipe.objects.count(), 2)
 
     def test_recipe_like_duplicate(self):
+        self.logger.info('Testing duplicate recipe like')
         RecipeLike.objects.create(user=self.user, recipe=self.recipe)
         response = self.client.post(f'/api/recipe/{self.recipe.id}/like/')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(RecipeLike.objects.filter(recipe=self.recipe).count(), 1)
 
     def test_recipe_dislike(self):
+        self.logger.info('Testing recipe dislike')
         RecipeLike.objects.create(user=self.user, recipe=self.recipe)
         response = self.client.delete(f'/api/recipe/{self.recipe.id}/like/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(RecipeLike.objects.filter(recipe=self.recipe).count(), 0)
 
     def test_recipe_dislike_not_liked(self):
+        self.logger.info('Testing bad request recipe like')
         response = self.client.delete(f'/api/recipe/{self.recipe.id}/like/')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(RecipeLike.objects.filter(recipe=self.recipe).count(), 0)
